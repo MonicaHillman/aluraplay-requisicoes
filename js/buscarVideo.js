5206fd94754d6a5d6e62f0d6d8309627d49399b8
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 16:45:28 -0300

    5.3 - Busca não encontrada

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -14,6 +14,10 @@ async function buscarVideo(evento) {
 
     busca.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+
+    if (busca.length == 0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
+    }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
