69ab8ca385a2a7967b01198328f1303bc4611cd8
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 16:39:55 -0300

    5.1 - Carregamento da lista

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -19,9 +19,13 @@ export default function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideos() {
-    const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    try {
+        const listaApi = await conectaApi.listaVideos();
+        listaApi.forEach(elemento => lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    } catch {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`
+    }
 }
 
 listaVideos();
