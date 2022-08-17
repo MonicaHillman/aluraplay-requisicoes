1749c2546869c4f0c45928539bb95f50fdaf79a5
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 15:16:05 -0300

    2.3 - ForEach

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,17 +1,27 @@
+import { conectaApi } from "./conectaApi.js";
+
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard() {
+function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = `<iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-    title="YouTube video player" frameborder="0"
+    video.innerHTML = `<iframe width="100%" height="72%" src="${elemento.url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
 <div class="descricao-video">
-    <img src="./img/logo.png" alt="logo canal alura">
-    <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-    <p>236 mil visualizações</p>
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
 </div>`
 
     return video;
 }
+
+async function listaVideos() {
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+}
+
+listaVideos();
