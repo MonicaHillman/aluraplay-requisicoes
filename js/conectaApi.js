1bd34ed4ac6967276ff0c3bccfc549e7d4be4f2c
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 16:42:20 -0300

    5.2 - Inserção de vídeos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -18,7 +18,9 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     });
-
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
     const conexaoConvertida = conexao.json();
 
     return conexaoConvertida;
--- js/criarVideo.js
+++ js/criarVideo.js
@@ -8,9 +8,13 @@ async function criarVideo(evento) {
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+    try {
+        await conectaApi.criaVideo(titulo, descricao, url, imagem);
 
-    await conectaApi.criaVideo(titulo, descricao, url, imagem);
-    window.location.href = "../pages/envio-concluido.html";
+        window.location.href = "../pages/envio-concluido.html";
+    } catch (e) {
+        alert(e);
+    }
 }
 
 formulario.addEventListener("submit", evento => criarVideo(evento))
