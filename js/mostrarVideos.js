19315fab27c9d44590e3afa81e86879f74209d13
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 16:20:25 -0300

    Ajuste 2.3

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -5,7 +5,7 @@ const lista = document.querySelector("[data-lista]");
 function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = `<iframe width="100%" height="72%" src="${elemento.url}"
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
     title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
