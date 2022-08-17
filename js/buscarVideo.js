d1b02e13fbd290e8b2eb1b5da00f8a8ba9f64bba
MonicaHillman <mmhillman1@ucs.br>
2022-08-17 16:35:18 -0300

    4.4 - Lista filtrada

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -8,6 +8,10 @@ async function buscarVideo(evento) {
 
     const lista = document.querySelector("[data-lista]");
 
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
     busca.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
